2c65f89b25507d756ef9ee7c3f883cb8d71a03a4
santhosS02 <santhoshs.0209@gmail.com>
2024-11-30 22:22:47 +0530

    List add

---
+++ collections/src/collections/Lists.java
@@ -0,0 +1,14 @@
+package collections;
+
+public class Lists {
+
+	public static void main(String[] args) {
+		// Array list 
+		// linked list 
+		// vector 
+		// stack 
+		System.out.println("This are the collections ");
+
+	}
+
+}
